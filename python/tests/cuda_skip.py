c61acdcdedf27cc8e087a2d6261cfa05eb004307
Awni Hannun <awni@apple.com>
2025-10-23 15:00:04 +0000

    add very basic fp qmv

--- python/tests/cuda_skip.py
+++ python/tests/cuda_skip.py
@@ -48,7 +48,6 @@ cuda_skip = {
     "TestQuantized.test_qmm_shapes",
     "TestQuantized.test_qmm_vjp",
     "TestQuantized.test_qmv",
-    "TestQuantized.test_mxfp4_qmv",
     "TestQuantized.test_mxfp4_qvm",
     "TestQuantized.test_qvm",
     "TestQuantized.test_qvm_splitk",
